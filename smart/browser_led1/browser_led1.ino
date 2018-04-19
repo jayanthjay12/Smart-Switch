4512c0b2066723d7d9484b957a41c06a1dc67ce1
jayanthjay12 <37666455+jayanthjay12@users.noreply.github.com>
2018-04-19 18:09:43 +0530

    Add files via upload

---
+++ smart/browser_led1/browser_led1.ino
@@ -0,0 +1,79 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "your wifi id";
+const char* password = " wifi pass key";
+unsigned char status_led=0;
+int LED=2;
+WiFiServer server(80);
+void setup() {
+  Serial.begin(57600);
+  pinMode(LED, OUTPUT);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  Serial.println("new client");
+  while(!client.available())
+  {
+    delay(1);
+  }
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  if (req.indexOf("/ledon") != -1)
+  {
+    status_led=0;   
+    digitalWrite(LED,LOW);
+    Serial.println("LED OFF");
+  }
+  else if(req.indexOf("/ledoff") != -1)
+  {
+    status_led=1;
+    digitalWrite(LED,HIGH);
+    Serial.println("LED ON");
+  }
+  String web = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  web += "<html>\r\n";
+  web += "<body>\r\n";
+  web += "<h1>LED Status</h1>\r\n";
+  web += "<p>\r\n";
+  if(status_led==1)
+      web += "LED Off\r\n";
+  else
+      web += "LED On\r\n";
+  web += "</p>\r\n";
+  web += "</p>\r\n";
+  web += "<a href=\"/ledon\">\r\n";
+  web += "<button>LED On</button >\r\n";
+  web += "</a>\r\n";
+  web += "</p>\r\n";
+  
+  web += "<a href=\"/ledoff\">\r\n";
+  web += "<button>LED Off</button >\r\n";
+  web += "</a>\r\n";
+  
+  web += "</body>\r\n";
+  web += "</html>\r\n";
+  
+  client.print(web);
+}
+
+
